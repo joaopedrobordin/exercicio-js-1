50640af94ecdc4fe89773c1394cdec8e9dcc0029
joaopedrobordin <73653589+joaopedrobordin@users.noreply.github.com>
2021-09-14 22:35:24 -0300

    ...

---
+++ funcoes.js
@@ -0,0 +1,215 @@
+let janeiro =  100
+let fevereiro = 300
+let marco = 90
+
+let valorTotal = janeiro + fevereiro + marco;
+    console.log(valorTotal);
+
+//
+
+let trabalhoDeHistoria = 8.0
+let trabalhoDeMatematica = 7.0
+let trabalhoDeCiencia = 10
+let trabalhoDeGeografia = 9.5
+
+let notaTotal = trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia;
+  mediaFinal = notaTotal / 4
+  console.log(mediaFinal)
+
+//
+
+let nome = catarina
+let sobrenome = paysandu
+let trabalho = militar
+let cartaoDeVisita = [${nome} , ${sobrenome} , ${trabalho}];
+
+//
+ 
+let numeroA = 13
+let numeroB = 46
+const ABsomado = numeroA + numeroB
+const ABsubtraido = numeroB - numeroA
+const ABdividido = numeroA / numeroB
+const ABmultiplicado = numeroA * numeroB
+
+//-------------------------------------Estrutura de decisão---------------------------------
+
+let idade = 40
+
+if (idade > 65){
+    console.log("Você já pode se aposentar")
+}else{
+    console.log("Você ainda não pode se aposentar")
+}
+
+//
+
+let nome = "João"
+let idade = 17
+
+console.log("Olá, "+nome)
+if(idade>=18){
+    console.log("Você passou no nosso teste e já pode dirigir!")
+}
+
+//
+ 
+let nome = "Joã"
+let idade = 17
+
+console.log("Olá, "+nome)
+if(idade>=18){
+    console.log("!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
