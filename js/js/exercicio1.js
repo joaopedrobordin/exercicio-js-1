30dde104a5d75b384267ec6be543428291957d66
joaopedrobordin <73653589+joaopedrobordin@users.noreply.github.com>
2021-09-16 22:31:16 -0300

    parte js

---
+++ js/js/exercicio1.js
@@ -0,0 +1,6 @@
+let janeiro =  100
+let fevereiro = 300
+let marco = 90
+
+let valorTotal = janeiro + fevereiro + marco;
+    console.log(valorTotal);
---
+++ js/js/exercicio12.js
@@ -0,0 +1,8 @@
+let produtoAtivo = true
+let produtoQtd = 12
+let venda = 1
+if(produtoQtd >= 1 && produtoAtivo == true){
+    console.log ("pode comprar")}
+    else{console.log("não esta disponivel")}
+
+
